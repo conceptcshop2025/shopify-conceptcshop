cde2795bde5fb86a95da5e04da231d2ee5c3fe75
Felipe Cancino <jf.cancino@conceptcshop.com>
2026-06-22 13:47:25 -0400

    bugfix and change logic to promotion cards with expiration

--- assets/custom-page-brand-marathon.js
+++ assets/custom-page-brand-marathon.js
@@ -1,15 +1,5 @@
 (function () {
   // Function to run when the page is ready
-  //const daysFormat = ['20260619', '20260621', '20260623', '20260625', '20260627', '20260629', '20260701', '20260703', '20260705', '20260707', '20260709', '20260711', '20260713', '20260715'];
-
-  function setDates(cardList) {
-    if (cardList.length > 0) {
-      cardList.forEach((card, index) => {
-        card.classList.add(`day-${daysFormat[index]}`);
-      });
-    }
-    return;
-  }
 
   function getActualDate() {
     const d = new Date();
@@ -21,15 +11,16 @@
 
   function initBrandMarathon() {
     const cardList = document.querySelector('.promotions-list-section').querySelectorAll('.item-card');
-    setDates(cardList);
     const actualDate = getActualDate();
     cardList.forEach((card) => {
-      const dayClass = Array.from(card.classList).find(cls => cls.startsWith('day-'));
-      const dayInCard = Number(dayClass.replace('day-', ''));
-      if (actualDate > dayInCard) {
-        card.classList.add('expired');
-      } else if (actualDate === dayInCard) {
-        card.classList.add('active-today');
+      const expirationDate = card.getAttribute('expiration-card');
+      const dayInCard = Number(expirationDate);
+      if (expirationDate) {
+        if (actualDate > dayInCard) {
+          card.classList.add('expired');
+        } else if (actualDate === dayInCard) {
+          card.classList.add('active-today');
+        }
       }
     });
   }
@@ -41,12 +32,6 @@
   } else {
     document.addEventListener('DOMContentLoaded', initBrandMarathon);
   }
-
-  // If you need to run something after all resources (images/fonts) are loaded, use window.load
-  window.addEventListener('load', function () {
-    // optional: run additional logic that requires full resource load
-    console.log('Window fully loaded');
-  });
 
   // Shopify-specific: re-run on section loads (useful in theme editor / AJAX page changes)
   document.addEventListener('shopify:section:load', initBrandMarathon);
